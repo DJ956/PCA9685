b107311b1a39f10a719fa88286053e237854e1b0
DJ <dexter13576@gmail.com>
2021-07-02 15:34:07 +0900

    ライブラリ記述

--- PCA9685.X/PCA9685.c
+++ PCA9685.X/PCA9685.c
@@ -3,24 +3,84 @@
 #include "i2c.h"
 #include "PCA9685.h"
 
-#define ADDR 0x40;
+#define PCA_ADDR 0x40
 
+#define SERVO_MIN 150
+
+#define SERVO_MAX 500
+
+uint8_t _read(){
+    uint8_t data;
+    i2c_start();
+    i2c_write(PCA_ADDR | 0x01);
+    data = i2c_read(PCA9685_MODE1);
+    
+    return data;
+}
+
+void _write(uint8_t addr, uint8_t d){
+    i2c_start();
+    i2c_write(PCA_ADDR);
+    i2c_write(addr);
+    i2c_write(d);
+    i2c_stop();
+}
+
+/**
+ * Init PCA9685 Device
+ */
 void init(){
+    i2c_start();
+    i2c_write(PCA_ADDR);
+    
+    i2c_write(0x0);
+    i2c_write(0x0);
+    
+    i2c_stop();
+}
+
+/**
+ * PCA9685 set pwm freq
+ * @param freq
+ */
+void set_pwm_freq(uint16_t freq){
+    uint16_t prescaleval = 6103;
+    prescaleval /= freq;
+    prescaleval -= 1;
+    
+    uint8_t prescale = (uint8_t)prescaleval;
+    uint8_t oldreg = _read();
+    uint8_t newreg = (oldreg | 0x7F) | 0x10;
+    _write(PCA9685_MODE1, newreg);
+    _write(PCA9685_PRESCALE, prescale);
+    _write(PCA9685_MODE1, oldreg);
+    __delay_ms(500);
+    _write(PCA9685_MODE1, oldreg | 0xA1);
     
 }
 
-void set_pwm_freq(uint8_t freq){
-    
-}
-
+/**
+ * transmission pwm to PCA9685
+ * @param num servo channel
+ * @param on 
+ * @param off angle
+ */
 void set_pwm(uint8_t num, uint16_t on, uint16_t off){
-    
+    i2c_start();
+    i2c_write(PCA_ADDR);
+    i2c_write(LED_ON_L + 4 * num);
+    i2c_write(on);
+    i2c_write(on >> 8);
+    i2c_write(off);
+    i2c_write(off >> 8);
+    i2c_stop();
 }
 
 uint8_t map(uint8_t ang, uint8_t target_min,  uint8_t target_max, uint8_t src_min, uint16_t src_max){
-    
+    return (ang - target_min) * (src_max - src_min) / (target_min - target_max) + src_min;
 }
 
 void servo_write(uint8_t ch, uint8_t ang){
-    
+    ang = map(ang, 0, 180, SERVO_MIN, SERVO_MAX);
+    set_pwm(ch, 0, ang);
 }
--- PCA9685.X/PCA9685.h
+++ PCA9685.X/PCA9685.h
@@ -10,6 +10,11 @@
 
 #include <xc.h> // include processor files - each processor file is guarded.  
 
+#define PCA9685_MODE1 0x0
+#define PCA9685_PRESCALE 0xFE
+
+#define LED_ON_L 0x06
+
 void init();
 void set_pwm_freq(uint8_t freq);
 void set_pwm(uint8_t num, uint16_t on, uint16_t off);
@@ -17,6 +22,9 @@ void set_pwm(uint8_t num, uint16_t on, uint16_t off);
 uint8_t map(uint8_t ang, uint8_t target_min,  uint8_t target_max, uint8_t src_min, uint16_t src_max);
 
 void servo_write(uint8_t ch, uint8_t ang);
+
+uint8_t _read(uint8_t address);
+void _write(uint8_t addr, uint8_t);
 
 #ifdef	__cplusplus
 extern "C" {
